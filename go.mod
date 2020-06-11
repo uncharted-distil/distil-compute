e988c28c0db58f3496b256c22290bd9d35e27305
philippe horne <phorne@uncharted.software>
2020-06-10 22:01:08 -0400

    Removed elastic related code.

--- go.mod
+++ go.mod
@@ -10,12 +10,13 @@ require (
 	github.com/mattn/go-colorable v0.1.4 // indirect
 	github.com/mattn/go-isatty v0.0.8
 	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b
-	github.com/olivere/elastic/v7 v7.0.15
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.5.1
 	github.com/unchartedsoftware/plog v0.0.0-20170413154239-34d2bbd3c0a9
 	github.com/vova616/xxhash v0.0.0-20130313230233-f0a9a8b74d48
 	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b
 	golang.org/x/sys v0.0.0-20191128015809-6d18c012aee9 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/genproto v0.0.0-20190425155659-357c62f0e4bb // indirect
 	google.golang.org/grpc v1.23.1
 )
--- metadata/metadata_test.go
+++ metadata/metadata_test.go
@@ -16,12 +16,8 @@
 package metadata
 
 import (
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
-	"github.com/Jeffail/gabs/v2"
-	elastic "github.com/olivere/elastic/v7"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -50,58 +46,4 @@ func TestMetadataFromSchema(t *testing.T) {
 	assert.Equal(t, meta.DataResources[0].Variables[3].Role, []string{"index"})
 	assert.Equal(t, meta.DataResources[0].Variables[3].Type, "integer")
 	assert.Equal(t, meta.DataResources[0].Variables[3].OriginalType, "integer")
-}
-
-func TestIngestMetadata(t *testing.T) {
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		reqBody, err := gabs.ParseJSONBuffer(r.Body)
-		assert.NoError(t, err)
-		assert.Equal(t, reqBody.Path("datasetName").Data().(string), "test dataset")
-		assert.Equal(t, reqBody.Path("datasetID").Data().(string), "test_dataset")
-		assert.Equal(t, reqBody.Path("description").Data().(string), string("YOU ARE STANDING AT THE END OF A ROAD BEFORE A SMALL BRICK BUILDING."))
-
-		variables := reqBody.Path("variables").Children()
-		assert.NotNil(t, variables)
-		assert.Equal(t, 4, len(variables))
-		assert.Equal(t, "bravo", variables[0].Path("colName").Data().(string))
-		roles := variables[0].Path("role").Data().([]interface{})
-		assert.Equal(t, "index", roles[0].(string))
-		assert.Equal(t, "integer", variables[0].Path("colType").Data().(string))
-		assert.Equal(t, "integer", variables[0].Path("colOriginalType").Data().(string))
-
-		assert.Equal(t, "alpha", variables[1].Path("colName").Data().(string))
-		roles = variables[1].Path("role").Data().([]interface{})
-		assert.Equal(t, "attribute", roles[0].(string))
-		assert.Equal(t, "string", variables[1].Path("colType").Data().(string))
-		assert.Equal(t, "string", variables[1].Path("colOriginalType").Data().(string))
-
-		assert.Equal(t, "whiskey", variables[2].Path("colName").Data().(string))
-		roles = variables[2].Path("role").Data().([]interface{})
-		assert.Equal(t, "suggestedTarget", roles[0].(string))
-		assert.Equal(t, "integer", variables[2].Path("colType").Data().(string))
-		assert.Equal(t, "integer", variables[2].Path("colOriginalType").Data().(string))
-
-		_, err = w.Write([]byte(`{
-				"_index":"test_index",
-				"_type":"metadata",
-				"_id":"test_dataset",
-				"_version":1,
-				"_shards": {
-					"total":2,
-					"successful":1,
-					"failed":0
-				},
-				"created":true
-			}`))
-		assert.NoError(t, err)
-	}))
-
-	client, err := elastic.NewSimpleClient(elastic.SetURL(testServer.URL))
-	assert.NoError(t, err)
-
-	meta, err := LoadMetadataFromOriginalSchema("./testdata/datasetDoc.json", true)
-	assert.NoError(t, err)
-
-	err = IngestMetadata(client, "test_index", "", Seed, meta)
-	assert.NoError(t, err)
 }
