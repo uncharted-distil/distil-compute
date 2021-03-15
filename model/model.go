560a0001e1ad72afb1d260c8b2e3e4c824404419
Yohann Paris <yparis@uncharted.software>
2021-03-15 11:26:04 -0400

    tabultations issue fixed.

--- model/model.go
+++ model/model.go
@@ -111,8 +111,8 @@ const (
 	// VarDistilRoleMetadata is the distil role for metadata variables
 	VarDistilRoleMetadata = "metadata"
 	// VarDistilRoleAugmented is the distil role for variables created for UX, not for model creation. i.e. Outliers detection
-        VarDistilRoleAugmented = "augmented"
-        // VarDeleted flags whether the variable is deleted.
+	VarDistilRoleAugmented = "augmented"
+	// VarDeleted flags whether the variable is deleted.
 	VarDeleted = "deleted"
 	// VarImmutableField is the name for the flag indicating whether the variable is immutable.
 	VarImmutableField = "immutable"
