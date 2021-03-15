056bea825a5a153b4a372bf574831f37873c13c9
Yohann Paris <yparis@uncharted.software>
2021-03-15 11:21:43 -0400

    Added a new VarDistilRole name *VarDistilRoleAugmented* to add variables that are created to enhance the UX, but are not used for creating models.

--- model/model.go
+++ model/model.go
@@ -110,7 +110,9 @@ const (
 	VarDistilRoleGrouping = "grouping"
 	// VarDistilRoleMetadata is the distil role for metadata variables
 	VarDistilRoleMetadata = "metadata"
-	// VarDeleted flags whether the variable is deleted.
+	// VarDistilRoleAugmented is the distil role for variables created for UX, not for model creation. i.e. Outliers detection
+        VarDistilRoleAugmented = "augmented"
+        // VarDeleted flags whether the variable is deleted.
 	VarDeleted = "deleted"
 	// VarImmutableField is the name for the flag indicating whether the variable is immutable.
 	VarImmutableField = "immutable"
